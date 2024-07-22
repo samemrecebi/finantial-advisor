6fc914d6603f6ef0ca558a4944a7610c60e23f98
Emre Cebi <emre@cebi.io>
2024-07-22 14:29:06 +0300

    Bazi Buglar duzeltildi

--- frontend/app/chat-screen/page.tsx
+++ frontend/app/chat-screen/page.tsx
@@ -1,150 +1,73 @@
-'use client';
-
-import { useState } from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faArrowUp,
-  faBars,
-  faUser,
-  faRobot,
-} from '@fortawesome/free-solid-svg-icons';
-import { Button, Drawer } from 'flowbite-react';
 
-function ChatPage() {
-  const router = useRouter();
-  const [messages, setMessages] = useState<
-  { text: string; isUser: boolean }[]
-  >([]);
-
-  const [input, setInput] = useState('');
-
-  const sendMessage = async () => {
-    if (input.trim() !== '') {
-      const userMessage = { text: input, isUser: true };
-      setMessages([...messages, userMessage]);
-      setInput('');
-
-      // Send the message to the backend API
-      try {
-        const response = await fetch('/api/chat', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ messages: [...messages, userMessage] }),
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          console.log('Received response:', data);
-          const botMessage = data.choices[0].message;
-          setMessages([...messages, userMessage, { text: botMessage.content, isUser: false }]);
-        } else {
-          console.error('Failed to get a response from the backend');
-        }
-      } catch (error) {
-        console.error('Error sending message:', error);
-      }
-    }
-  };
-
-  const redirectToProfile = () => {
-    router.push('/profile'); // Adjust this route according to your Next.js setup
-  };
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClose = () => setIsOpen(false);
-
+function HomePage() {
   return (
-    <div className="flex flex-col h-screen bg-blue-100 font-sans">
-      {/* Header */}
-      <div className="flex items-center justify-between p-4">
-        <Button className="bg-blue-900" onClick={() => setIsOpen(true)}>
-          <FontAwesomeIcon icon={faBars} />
-        </Button>
-        <div className="flex items-center space-x-4">
-          <button
-            type="button"
-            className="p-2 bg-blue-900 rounded-md"
-            onClick={redirectToProfile}
-          >
-            <FontAwesomeIcon icon={faUser} />
-          </button>
+    <div className="min-h-screen flex flex-col text-center bg-gradient-to-r from-indigo-200 to-indigo-400 shadow-lg">
+      <header className="bg-gradient-to-r from-indigo-950 to-indigo-400 shadow-lg">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex-1 md:flex md:items-center md:gap-12">
+              <a className="block text-blue-200 no-underline hover:opacity-80" href="/">
+                <span className="sr-only">Home</span>
+                <div className="flex items-center space-x-2">
+                  <Image src="/logo.png" alt="Fintech" width={100} height={100} />
+                  <div className="flex flex-col">
+                    <div className="text-white text-sm font-bold">Fintech</div>
+                    <div className="text-white text-[10px] font-bold">Finansal Asistanınız</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="md:flex md:items-center md:gap-12">
+              <nav aria-label="Global" className="hidden md:block h-5">
+                <ul className="flex items-center gap-6 text-sm">
+                  <li>
+                    <a className="no-underline text-blue-100 transition hover:text-gray-500/75" href="/">
+                      Ana Sayfa
+                    </a>
+                  </li>
+                  <li>
+                    <a className="no-underline text-blue-100 transition hover:text-gray-500/75" href="/features">
+                      Özellikler
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="flex items-center gap-4">
+                <div className="sm:flex sm:gap-4">
+                  <a className="rounded-md bg-blue-100 px-5 py-2.5 text-sm font-medium text-blue-600 shadow no-underline transition hover:bg-blue-200" href="/signin">
+                    Giriş Yap
+                  </a>
+                  <div className="hidden sm:flex">
+                    <a className="rounded-md bg-blue-100 px-5 py-2.5 text-sm font-medium text-blue-600 no-underline transition hover:bg-blue-200" href="/signup">
+                      Kayıt Ol
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <Drawer className="bg-blue-600" open={isOpen} onClose={handleClose}>
-        <Drawer.Header className="text-blue-100" title="Konuşmalar" />
-        <Drawer.Items>
-          <div className="flex items-center justify-between">
-            <a
-              href="/chat-screen"
-              className="inline-flex items-center rounded-lg bg-blue-900 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-            >
-              Yeni Konusma
+      </header>
+      <div className="md:p-12 lg:px-16 lg:py-24">
+        <div className="mx-auto my-48 max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+          <h2 className="text-2xl font-bold text-blue-900 md:text-4xl">
+            Bir sonraki yatırımlarınızı ve daha fazlasını yeni Finansal AI asistanınımız ile bulun.
+          </h2>
+          <p className="hidden text-blue-600 md:mt-4 md:block">
+            Başlamak için kayıt olun.
+          </p>
+          <div className="mt-4 md:mt-8">
+            <a href="/signup" className="inline-block rounded bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-blue-700 focus:outline-none no-underline focus:ring focus:ring-yellow-400">
+              Kayıt ol
             </a>
           </div>
-        </Drawer.Items>
-      </Drawer>
-
-      {/* Chat Area */}
-      <div className="flex-1 p-4 overflow-y-auto">
-        {messages.length === 0 ? (
-          <div className="flex justify-center items-center h-full">
-            <Image src="/logo.png" alt="Fintech" width={300} height={400} />
-          </div>
-        ) : (
-          messages.map((message, index) => (
-            <div
-              key={index}
-              className={`flex items-start mb-4 ${message.isUser ? 'justify-end' : 'justify-start'}`}
-            >
-              {!message.isUser && (
-                <FontAwesomeIcon
-                  icon={faRobot}
-                  style={{ color: '#1E3A8A' }}
-                  className="p-2"
-                />
-              )}
-              <div
-                className={`p-2 bg-blue-200 rounded-lg max-w-md ${message.isUser ? 'ml-2' : 'mr-2'}`}
-              >
-                <p className="text-blue-900 text-xl">{message.text}</p>
-              </div>
-              {message.isUser && (
-                <FontAwesomeIcon
-                  icon={faUser}
-                  style={{ color: '#1E3A8A' }}
-                  className="p-2"
-                />
-              )}
-            </div>
-          ))
-        )}
+        </div>
       </div>
-
-      {/* Input Area */}
-      <div className="flex items-center p-4 ">
-        <input
-          type="text"
-          placeholder=" Mesajınızı buraya giriniz"
-          className="flex-1 text-[32px] bg-blue-200 text-blue-900 placeholder-blue-400 p-2 rounded-md"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
-        />
-        <button
-          type="submit"
-          className="p-2 ml-2 bg-blue-900 rounded-md hover:bg-blue-600"
-          onClick={sendMessage}
-        >
-          <FontAwesomeIcon icon={faArrowUp} />
-        </button>
-      </div>
+      <footer className="mt-auto bg-gradient-to-r from-indigo-950 to-indigo-400 shadow-lg py-4">
+        <p className="text-center text-white">© Fintech Financial Assistant 2024 All rights reserved</p>
+      </footer>
     </div>
   );
 }
-
-export default ChatPage;
+export default HomePage;
--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -2,99 +2,100 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const ProfilePage = () => {
-    return (
-        <div className="min-h-screen flex justify-end items-center body">
-            <div className="bg-[#D6E4FF] mr-0">
-                <svg width="100vw" height="100vh">
-                    <defs>
-                        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                            <stop offset="0%" stopColor="#1D39C4" /> 
-                            <stop offset="100%" stopColor="#030852" />
-                        </linearGradient>
-                    </defs>
-                    <circle cx="1250" cy="350" r="1000" fill="url(#gradient)" />
-                    <circle cx="50vw" cy="12vw" r="120" fill="#FFFFFF" />
-                    <foreignObject x="35" y="40" width="50px" height="50px">
-                        <div>
-                            <Image
-                                src="/menu.png"
-                                alt="menu"
-                                width={40} 
-                                height={40} 
-                            />
-                        </div>
-                    </foreignObject>
-                    <foreignObject x="43%" y="40%" width="1000px" height="40px">
-                        <div className="text-white text-xl font-bold">
-                            Risk-Averse Investor
-                        </div>
-                    </foreignObject>
-                    <foreignObject x="41%" y="53%" width="240px" height="40px">
-                        <div className="relative">
-                            <label className="absolute left-0 -top-6 text-white">First Name</label>
-                            <input
-                                type="text"
-                                placeholder="Paul"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left" style={{ backgroundColor: '#D6E4FF' }}
-                                disabled
-                            />
-                        </div>
-                    </foreignObject>
-                    <foreignObject x="41%" y="64%" width="240px" height="40px">
-                        <div className="relative">
-                            <label className="absolute left-0 -top-6 text-white">Last Name</label>
-                            <input
-                                type="text"
-                                placeholder="Smith"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left" style={{ backgroundColor: '#D6E4FF' }}
-                                disabled
-                            />
-                        </div>
-                    </foreignObject>
-                    <foreignObject x="41%" y="75%" width="240px" height="40px">
-                        <div className="relative">
-                            <label className="absolute left-0 -top-6 text-white">Username</label>
-                            <input
-                                type="text"
-                                placeholder="paulsmith"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left" 
-                                style={{ backgroundColor: '#D6E4FF' }}
-                                disabled
-                            />
-                        </div>
-                    </foreignObject>
-                    <foreignObject x="41%" y="86%" width="240px" height="40px">
-                        <div className="relative">
-                            <label className="absolute left-0 -top-6 text-white">Password</label>
-                            <input
-                                type="password"
-                                placeholder="***"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"                                 
-                                style={{ backgroundColor: '#D6E4FF' }}
-                                disabled
-                            />
-                        </div>
-                    </foreignObject>
-                    <foreignObject x="90%" y="88%" width="300px" height="100px">
-                        <Link href="/profileedit">
-                            
-                                <Image
-                                    src="/edit.png" // Ensure this path is correct
-                                    alt="Edit Profile"
-                                    width={100} // Adjust the width as needed
-                                    height={100} // Adjust the height as needed
-                                    className="cursor-pointer"
-                                />
-                            
-                        </Link>
-                    </foreignObject>
-
-
-                </svg>
+function ProfilePage() {
+  return (
+    <div className="min-h-screen flex justify-end items-center body">
+      <div className="bg-[#D6E4FF] mr-0">
+        <svg width="100vw" height="100vh">
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="0%" stopColor="#1D39C4" />
+              <stop offset="100%" stopColor="#030852" />
+            </linearGradient>
+          </defs>
+          <circle cx="1250" cy="350" r="1000" fill="url(#gradient)" />
+          <circle cx="50vw" cy="12vw" r="120" fill="#FFFFFF" />
+          <foreignObject x="35" y="40" width="50px" height="50px">
+            <div>
+              <Image
+                src="/menu.png"
+                alt="menu"
+                width={40}
+                height={40}
+              />
             </div>
-        </div>
-    );
-};
+          </foreignObject>
+          <foreignObject x="43%" y="40%" width="1000px" height="40px">
+            <div className="text-white text-xl font-bold">
+              Risk-Averse Investor
+            </div>
+          </foreignObject>
+          <foreignObject x="41%" y="53%" width="240px" height="40px">
+            <div className="relative">
+              <label className="absolute left-0 -top-6 text-white">First Name</label>
+              <input
+                type="text"
+                placeholder="Paul"
+                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                style={{ backgroundColor: '#D6E4FF' }}
+                disabled
+              />
+            </div>
+          </foreignObject>
+          <foreignObject x="41%" y="64%" width="240px" height="40px">
+            <div className="relative">
+              <label className="absolute left-0 -top-6 text-white">Last Name</label>
+              <input
+                type="text"
+                placeholder="Smith"
+                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                style={{ backgroundColor: '#D6E4FF' }}
+                disabled
+              />
+            </div>
+          </foreignObject>
+          <foreignObject x="41%" y="75%" width="240px" height="40px">
+            <div className="relative">
+              <label className="absolute left-0 -top-6 text-white">Username</label>
+              <input
+                type="text"
+                placeholder="paulsmith"
+                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                style={{ backgroundColor: '#D6E4FF' }}
+                disabled
+              />
+            </div>
+          </foreignObject>
+          <foreignObject x="41%" y="86%" width="240px" height="40px">
+            <div className="relative">
+              <label className="absolute left-0 -top-6 text-white">Password</label>
+              <input
+                type="password"
+                placeholder="***"
+                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                style={{ backgroundColor: '#D6E4FF' }}
+                disabled
+              />
+            </div>
+          </foreignObject>
+          <foreignObject x="90%" y="88%" width="300px" height="100px">
+            <Link href="/profileedit">
+
+              <Image
+                src="/edit.png" // Ensure this path is correct
+                alt="Edit Profile"
+                width={100} // Adjust the width as needed
+                height={100} // Adjust the height as needed
+                className="cursor-pointer"
+              />
+
+            </Link>
+          </foreignObject>
+
+        </svg>
+      </div>
+    </div>
+  );
+}
 
 export default ProfilePage;
