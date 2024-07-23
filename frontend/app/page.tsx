067ba945311eee3190a5552146a5307e60440f0c
Melis <melis086@gmail.com>
2024-07-23 13:54:25 +0300

    update pages.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,72 +1,133 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
 import Image from 'next/image';
-function HomePage() {
-  return (
-    <div className="min-h-screen flex flex-col text-center bg-gradient-to-r from-indigo-200 to-indigo-400 shadow-lg">
-      <header className="bg-gradient-to-r from-indigo-950 to-indigo-400 shadow-lg">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex-1 md:flex md:items-center md:gap-12">
-              <a className="block text-blue-200 no-underline hover:opacity-80" href="/">
-                <span className="sr-only">Home</span>
-                <div className="flex items-center space-x-2">
-                  <Image src="/logo.png" alt="Fintech" width={100} height={100} />
-                  <div className="flex flex-col">
-                    <div className="text-white text-sm font-bold">Fintech</div>
-                    <div className="text-white text-[10px] font-bold">Finansal Asistanınız</div>
-                  </div>
-                </div>
-              </a>
+
+const ProfilePage = () => {
+    const [user, setUser] = useState({
+        first_name: '',
+        last_name: '',
+        username: '',
+        risk_degree: '',
+    });
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const token = localStorage.getItem('token');
+            const username = localStorage.getItem('username'); // Assuming username is stored in localStorage
+
+            if (!token || !username) return;
+
+            try {
+                const response = await axios.post('http://localhost:3001/api/users/getUserDetails', 
+                    { username }, // Pass username in the request body
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                setUser(response.data);
+            } catch (error) {
+                console.error("Failed to fetch user data:", error);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    return (
+        <div className="min-h-screen flex justify-end items-center body">
+            <div className="bg-[#D6E4FF] mr-0">
+                <svg width="100vw" height="100vh">
+                    <defs>
+                        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                            <stop offset="0%" stopColor="#1D39C4" /> 
+                            <stop offset="100%" stopColor="#030852" />
+                        </linearGradient>
+                    </defs>
+                    <circle cx="1250" cy="350" r="1000" fill="url(#gradient)" />
+                    <circle cx="50vw" cy="12vw" r="120" fill="#FFFFFF" />
+                    <foreignObject x="35" y="40" width="50px" height="50px">
+                        <div>
+                            <Image
+                                src="/menu.png"
+                                alt="menu"
+                                width={40} 
+                                height={40} 
+                            />
+                        </div>
+                    </foreignObject>
+                    <foreignObject x="43%" y="40%" width="1000px" height="40px">
+                        <div className="text-white text-xl font-bold">
+                            {user.risk_degree}
+                        </div>
+                    </foreignObject>
+                    <foreignObject x="41%" y="53%" width="240px" height="40px">
+                        <div className="relative">
+                            <label className="absolute left-0 -top-6 text-white">First Name</label>
+                            <input
+                                type="text"
+                                placeholder={user.first_name}
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                                style={{ backgroundColor: '#D6E4FF' }}
+                                disabled
+                            />
+                        </div>
+                    </foreignObject>
+                    <foreignObject x="41%" y="64%" width="240px" height="40px">
+                        <div className="relative">
+                            <label className="absolute left-0 -top-6 text-white">Last Name</label>
+                            <input
+                                type="text"
+                                placeholder={user.last_name}
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                                style={{ backgroundColor: '#D6E4FF' }}
+                                disabled
+                            />
+                        </div>
+                    </foreignObject>
+                    <foreignObject x="41%" y="75%" width="240px" height="40px">
+                        <div className="relative">
+                            <label className="absolute left-0 -top-6 text-white">Username</label>
+                            <input
+                                type="text"
+                                placeholder={user.username}
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left" 
+                                style={{ backgroundColor: '#D6E4FF' }}
+                                disabled
+                            />
+                        </div>
+                    </foreignObject>
+                    <foreignObject x="41%" y="86%" width="240px" height="40px">
+                        <div className="relative">
+                            <label className="absolute left-0 -top-6 text-white">Password</label>
+                            <input
+                                type="password"
+                                placeholder="***"
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"                                 
+                                style={{ backgroundColor: '#D6E4FF' }}
+                                disabled
+                            />
+                        </div>
+                    </foreignObject>
+                    <foreignObject x="90%" y="88%" width="300px" height="100px">
+                        <Link href="/profileedit">
+                            <Image
+                                src="/edit.png" // Ensure this path is correct
+                                alt="Edit Profile"
+                                width={100} // Adjust the width as needed
+                                height={100} // Adjust the height as needed
+                                className="cursor-pointer"
+                            />
+                        </Link>
+                    </foreignObject>
+                </svg>
             </div>
-            <div className="md:flex md:items-center md:gap-12">
-              <nav aria-label="Global" className="hidden md:block h-5">
-                <ul className="flex items-center gap-6 text-sm">
-                  <li>
-                    <a className="no-underline text-blue-100 transition hover:text-gray-500/75" href="/">
-                      Ana Sayfa
-                    </a>
-                  </li>
-                  <li>
-                    <a className="no-underline text-blue-100 transition hover:text-gray-500/75" href="/features">
-                      Özellikler
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-              <div className="flex items-center gap-4">
-                <div className="sm:flex sm:gap-4">
-                  <a className="rounded-md bg-blue-100 px-5 py-2.5 text-sm font-medium text-blue-600 shadow no-underline transition hover:bg-blue-200" href="/signin">
-                    Giriş Yap
-                  </a>
-                  <div className="hidden sm:flex">
-                    <a className="rounded-md bg-blue-100 px-5 py-2.5 text-sm font-medium text-blue-600 no-underline transition hover:bg-blue-200" href="/signup">
-                      Kayıt Ol
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </header>
-      <div className="md:p-12 lg:px-16 lg:py-24">
-        <div className="mx-auto my-48 max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-          <h2 className="text-2xl font-bold text-blue-900 md:text-4xl">
-            Bir sonraki yatırımlarınızı ve daha fazlasını yeni Finansal AI asistanınımız ile bulun.
-          </h2>
-          <p className="hidden text-blue-600 md:mt-4 md:block">
-            Başlamak için kayıt olun.
-          </p>
-          <div className="mt-4 md:mt-8">
-            <a href="/signup" className="inline-block rounded bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-blue-700 focus:outline-none no-underline focus:ring focus:ring-yellow-400">
-              Kayıt ol
-            </a>
-          </div>
-        </div>
-      </div>
-      <footer className="mt-auto bg-gradient-to-r from-indigo-950 to-indigo-400 shadow-lg py-4">
-        <p className="text-center text-white">© Fintech Financial Assistant 2024 All rights reserved</p>
-      </footer>
-    </div>
-  );
-}
-export default HomePage;
+    );
+};
+
+export default ProfilePage;
--- frontend/app/profileedit/page.tsx
+++ frontend/app/profileedit/page.tsx
@@ -1,11 +1,74 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const ProfileEditPage = () => {
+    const [formData, setFormData] = useState({
+        first_name: '',
+        last_name: '',
+        username: '',
+        password: '',
+        risk_degree: '', // Added risk_degree field
+    });
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const token = localStorage.getItem('token');
+            const username = localStorage.getItem('username');
+            if (!token || !username) return;
+
+            try {
+                const response = await axios.post('http://localhost:3001/api/users/getUserDetails', { username }, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                setFormData({
+                    first_name: response.data.first_name,
+                    last_name: response.data.last_name,
+                    username: response.data.username,
+                    password: '', // It is generally not safe to show the password in the form
+                    risk_degree: response.data.risk_degree, // Added risk_degree field
+                });
+            } catch (error) {
+                console.error("Failed to fetch user data:", error);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+
+        try {
+            await axios.put('http://localhost:3001/api/users/update', formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            alert('Profile updated successfully!');
+        } catch (error) {
+            console.error("Failed to update profile:", error);
+            alert('Failed to update profile');
+        }
+    };
+
     return (
         <div className="min-h-screen flex justify-end items-center body">
-
             <div className="bg-[#D6E4FF] mr-0">
                 <svg width="100vw" height="100vh">
                     <defs>
@@ -31,8 +94,12 @@ const ProfileEditPage = () => {
                             <label className="absolute left-0 -top-6 text-white">First Name</label>
                             <input
                                 type="text"
+                                name="first_name"
+                                value={formData.first_name}
+                                onChange={handleChange}
                                 placeholder="Paul"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left" style={{ backgroundColor: '#D6E4FF' }}
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                                style={{ backgroundColor: '#D6E4FF' }}
                             />
                         </div>
                     </foreignObject>
@@ -41,8 +108,12 @@ const ProfileEditPage = () => {
                             <label className="absolute left-0 -top-6 text-white">Last Name</label>
                             <input
                                 type="text"
+                                name="last_name"
+                                value={formData.last_name}
+                                onChange={handleChange}
                                 placeholder="Smith"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left" style={{ backgroundColor: '#D6E4FF' }}
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                                style={{ backgroundColor: '#D6E4FF' }}
                             />
                         </div>
                     </foreignObject>
@@ -51,8 +122,12 @@ const ProfileEditPage = () => {
                             <label className="absolute left-0 -top-6 text-white">Username</label>
                             <input
                                 type="text"
-                                placeholder="paulsmith"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"                                 style={{ backgroundColor: '#D6E4FF' }}
+                                name="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                placeholder={formData.username}
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                                style={{ backgroundColor: '#D6E4FF' }}
                             />
                         </div>
                     </foreignObject>
@@ -61,31 +136,50 @@ const ProfileEditPage = () => {
                             <label className="absolute left-0 -top-6 text-white">Password</label>
                             <input
                                 type="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
                                 placeholder="***"
-                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"                                 style={{ backgroundColor: '#D6E4FF' }}
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                                style={{ backgroundColor: '#D6E4FF' }}
                             />
                         </div>
                     </foreignObject>
-
-                    {/* Save and Cancel Buttons */}
+                    <foreignObject x="41%" y="97%" width="240px" height="40px">
+                        <div className="relative">
+                            <label className="absolute left-0 -top-6 text-white">Risk Degree</label>
+                            <input
+                                type="text"
+                                name="risk_degree"
+                                value={formData.risk_degree}
+                                onChange={handleChange}
+                                placeholder="Low"
+                                className="w-full h-full px-4 py-2 text-black rounded border border-gray-300 focus:outline-none focus:border-blue-500 text-left"
+                                style={{ backgroundColor: '#D6E4FF' }}
+                            />
+                        </div>
+                    </foreignObject>
                     <foreignObject x="92%" y="90%" width="300px" height="100px">
-                        
-                                <Image
-                                    src="/save.png" 
-                                    alt="Save Profile"
-                                    width={40} 
-                                    height={40}
-                                    className="cursor-pointer"
-                                />
+                        <div onClick={handleSubmit}>
+                            <Image
+                                src="/save.png" 
+                                alt="Save Profile"
+                                width={40} 
+                                height={40}
+                                className="cursor-pointer"
+                            />
+                        </div>
                     </foreignObject>
                     <foreignObject x="95%" y="90%" width="300px" height="100px">
-                                <Image
-                                    src="/cancel.png" 
-                                    alt="Cancel"
-                                    width={50} 
-                                    height={50} 
-                                    className="cursor-pointer"
-                                />
+                        <Link href="/">
+                            <Image
+                                src="/cancel.png" 
+                                alt="Cancel"
+                                width={50} 
+                                height={50} 
+                                className="cursor-pointer"
+                            />
+                        </Link>
                     </foreignObject>
                 </svg>
             </div>
