1e85b58efb48c31e86b6446d7c7894cf93c55a1c
Melis <melis086@gmail.com>
2024-07-23 15:58:41 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,12 @@ import currrenciesRouter from './controllers/CurrencyController.js';
 import users from './controllers/Users.js'
 import stockRouter from './controllers/StockController.js';
 import chatRouter from './controllers/chat.js';
+import cors from 'cors';
 
 
 const app = express();
-app.use(express.json())
-
+app.use(express.json());
+app.use(cors());
 
 //API routes
 app.use('/api/users', users)
