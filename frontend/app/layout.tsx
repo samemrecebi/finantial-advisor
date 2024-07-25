df5ab21b00b1045e032e9005a3d2262898c7fef5
BerkBelhan <berk.belhan@gmail.com>
2024-07-25 14:41:48 +0300

    Homepage, signin, signup geliştirildi son haline getirildi

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeModeScript } from 'flowbite-react';
 const poppins = Poppins({ weight: ['300', '400', '400'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Fintech',
+  title: 'Wealthify',
   description: 'Senin yatırım asistanın',
   icons: 'favicon.ico',
 };
--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -3,16 +3,16 @@ import Image from 'next/image';
 
 function SignInForm() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-dark text-white">
+    <main className="flex items-center justify-center min-h-screen bg-gradient-to-r from-white to-indigo-400 text-white">
       {/* Arka planı isterseniz sonra bir resim vs. ile değiştirebiliriz */}
-      <div className="flex flex-row bg-gradient-to-r from-indigo-950 to-indigo-400 rounded-lg shadow-lg max-w-3xl w-full">
+      <div className="flex flex-row bg-gradient-to-r from-indigo-950 to-indigo-400 rounded-lg shadow-lg max-w-4xl w-full">
         <div className="w-1/2 p-6">
           <div className="flex justify-between items-center mb-4">
             <div className="flex items-center">
-              <Image src="/logo.png" alt="Fintech" width={77} height={61} />
+              <Image src="/logo.png" alt="Wealthify" width={77} height={61} />
               <div>
-                <div className="text-lg font-bold">Fintech</div>
-                <div className="text-xs">Finans asistanınız</div>
+                <div className="text-lg font-bold">Wealthify</div>
+                <div className="text-xs">Finans Asistanınız</div>
               </div>
             </div>
           </div>
@@ -20,11 +20,11 @@ function SignInForm() {
           <form>
             <div className="mb-3">
               <label className="block text-sm font-medium mb-1">
-                E-posta
+                Kullanıcı Adı
                 <input
-                  type="email"
+                  type="text"
                   className="block w-full p-2 rounded border text-black"
-                  placeholder="kullanici.adi@ornek.com"
+                  placeholder="Kullanıcı Adı"
                 />
               </label>
             </div>
@@ -49,7 +49,7 @@ function SignInForm() {
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+              className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded mb-4"
             >
               Giriş Yap
             </button>
@@ -66,10 +66,10 @@ function SignInForm() {
           <h1 className="text-2xl font-bold mb-4">Tekrar Hoşgeldiniz!</h1>
           <p className="mb-4 text-sm">Giriş yaparak yolculuğunuza devam edin</p>
           <p className="mb-4 text-lg font-bold">VEYA</p>
-          <p className="mb-4 text-sm">E-postanızla kayıt olun</p>
+          <p className="mb-4 text-sm">Hemen kayıt olun</p>
           <Link
             href="/signup"
-            className="btn btn-lg btn-primary text-white font-bold py-2 px-4 rounded mb-4"
+            className="rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white no-underline transition hover:bg-indigo-700"
           >
             Kayıt Ol
           </Link>
